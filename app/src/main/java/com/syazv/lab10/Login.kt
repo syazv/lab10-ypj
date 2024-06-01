fb864b9ba2f738c318b3b50288c7db92c88cfd24
Muhammad Syazani <syazanifaudzi@gmail.com>
2024-06-01 15:38:04 +0800

    Update

--- app/src/main/java/com/syazv/lab10/Login.kt
+++ app/src/main/java/com/syazv/lab10/Login.kt
@@ -1,15 +1,20 @@
 package com.syazv.lab10
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.syazv.lab10.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +23,57 @@ class Login : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        // Dapatkan connection ke database
+        auth = FirebaseAuth.getInstance()
 
+        binding.loginLoginBtn.setOnClickListener {
+            if (binding.loginEmailEditText.text.trim().toString().isNotEmpty() ||
+                binding.loginPasswordEditText.text.trim().toString().isNotEmpty()) {
+
+                loginUser(
+                    binding.loginEmailEditText.text.toString(),
+                    binding.loginPasswordEditText.text.toString()
+                )
+
+            }
+            else{
+
+                Snackbar.make(binding.root,
+                    "Please check your username and password",
+                    Snackbar.LENGTH_LONG)
+                    .show()
+
+            }
+
+        }
     }
+
+    fun loginUser(email:String, password:String){
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener{
+            task ->
+            if (task.isSuccessful) {
+                val intent = Intent(this, ServiceActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                Snackbar.make(binding.root,
+                    "Please check your username and password",
+                    Snackbar.LENGTH_LONG)
+                    .show()
+            }
+
+        }
+            /* // Untuk check error log from Firebase default message
+            .addOnFailureListener { err ->
+                Log.d("debug", err.stackTraceToString())
+                Snackbar.make(binding.root,
+                    err.message!!,
+                    Snackbar.LENGTH_LONG)
+                    .show()
+            }*/
+    }
+
+
 }
+
+
--- app/src/main/java/com/syazv/lab10/Signup.kt
+++ app/src/main/java/com/syazv/lab10/Signup.kt
@@ -2,6 +2,7 @@ package com.syazv.lab10
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,10 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.syazv.lab10.databinding.ActivitySignupBinding
 
 class Signup : AppCompatActivity() {
@@ -16,6 +21,8 @@ class Signup : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
 
     private lateinit var auth: FirebaseAuth;
+
+    private lateinit var db:FirebaseFirestore;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,26 +35,52 @@ class Signup : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 
         binding.signupCreateBtn.setOnClickListener {
-            createUser(binding.signupEmailEditText.text.toString(), binding.signupPasswordEditText.text.toString())
+            createUser(binding.signupEmailEditText.text.toString(),
+                binding.signupPasswordEditText.text.toString())
         }
 
+        db = Firebase.firestore
     }
 
     fun createUser(email:String, password:String){
         // Create User menggunakan email dan password
-        //  Bila dah sudah saya dapat Task
+        //  Bila dah sudah saya akan dapat Task
         auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
-                task -> if(task.isSuccessful){
+            task -> if(task.isSuccessful){
+            newCustomer()
             val intent = Intent(this, ThankyouActivity::class.java)
             startActivity(intent)
-        }
+            }
         else{
             Snackbar.make(
                 binding.root,
                 "Enter a valid username and password",
                 Snackbar.LENGTH_LONG
             ).show()
+            }
         }
-        }
+    }
+
+    private fun newCustomer(){
+        // Key value based data type
+        // Name = Wan Muzaffar, Email = wanwan@gmail.com, Country : Malaysia
+        val customer = hashMapOf(
+            "name" to binding.signupNameEditText.text.toString().trim(),
+            "city" to binding.signupCityEditText.text.toString().trim(),
+            "country" to binding.signupCountryEditText.text.toString().trim(),
+            "phone" to binding.signupPhoneEditText.text.toString().trim(),
+        )
+
+        db.collection("customers")
+            .add(customer)
+            .addOnSuccessListener {
+                documentReference ->
+                Log.d("debug", "Document successfully added with id ${documentReference.id}")
+            }
+
+            .addOnFailureListener { e ->
+                Log.d("debug","An error happen ${e.message}")
+            }
+
     }
 }
